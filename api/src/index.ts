2dbff9062d6d4bd23c301016d0c02af0646dd09a
Adam Gobran <adamgobes@gmail.com>
2022-05-23 21:48:41 -0400

    script

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,8 +1,13 @@
 import express from "express";
 const app = express();
+const port = process.env.PORT || 3001;
 
 app.get("/", (req, res) =>
   res.json({
     message: "hello world",
   })
 );
+
+app.listen(port, () => {
+  console.log("server running...");
+});
